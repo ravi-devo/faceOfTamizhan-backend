0f32e3f10e1064684d3d57ccf0bf81db2f4c50b6
ravi-devo <ravi.ravichandran370@gmail.com>
2024-02-18 18:14:24 +0530

    CORS added

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const connectDB = require('./config/db');
 const userRoutes = require('./routes/userRoutes');
 const cookieParser = require('cookie-parser');
 const postRoutes = require('./routes/postRoutes');
+const cors = require('cors');
 
 dotenv.config();
 
@@ -12,6 +13,8 @@ const port = process.env.PORT;
 connectDB();
 
 const app = express();
+
+app.use(cors());
 
 app.use(express.json());
 
